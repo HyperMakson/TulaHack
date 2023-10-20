902d79c77812369f89865da2dd400bbe60daf571
HyperMakson <maks03work@gmail.com>
2023-10-20 21:00:15 +0300

    commit 4

--- handlers/start.py
+++ handlers/start.py
@@ -4,6 +4,7 @@ from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, C
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
+from keyboards.simple_keyboard import make_row_keyboard
 
 router = Router()
 
@@ -14,6 +15,7 @@ class Clinic(StatesGroup):
     user = State()
 
 specialization_arr = ["Терапевт", "Уролог", "Стоматолог", "Офтальмолог"]
+specialist_arr = ["Сорокин", "Цыбуля", "Генералов", "Митяев", "Данилов"]
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
@@ -28,11 +30,15 @@ async def cmd_start(message: Message):
 
 @router.callback_query(F.data == "appointment")
 async def start_appointment(callback: CallbackQuery, state: FSMContext):
-    await callback.message.answer("Это запись")
+    await callback.message.answer(text="Выберете специализацию врача", reply_markup=make_row_keyboard(specialization_arr))
     await state.set_state(Clinic.specialization)
     await callback.answer()
 
 @router.message(Clinic.specialization, F.text.in_(specialization_arr))
+async def specialization_chosen(message: Message, state: FSMContext):
+    await state.update_data(chosen_specialization=message.text.lower())
+    await message.answer(text="Хорошо. Выберете конкретного врача.", reply_markup=make_row_keyboard(specialist_arr))
+    await state.set_state(Clinic.specialist)
 
 @router.callback_query(F.data == "my_notes")
 async def start_appointment(callback: CallbackQuery):
