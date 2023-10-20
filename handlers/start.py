64360a61c381ed9769ada4199f084eb03da06e4b
HyperMakson <maks03work@gmail.com>
2023-10-20 20:53:25 +0300

    srgsgdg

--- handlers/start.py
+++ handlers/start.py
@@ -2,8 +2,18 @@ from aiogram import Router, F
 from aiogram.filters.command import Command
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 
 router = Router()
+
+class Clinic(StatesGroup):
+    specialization = State()
+    specialist = State()
+    date = State()
+    user = State()
+
+specialization_arr = ["Терапевт", "Уролог", "Стоматолог", "Офтальмолог"]
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
@@ -17,9 +27,12 @@ async def cmd_start(message: Message):
     await message.answer("Здравствуйте! Вы попали", reply_markup=keyboard)
 
 @router.callback_query(F.data == "appointment")
-async def start_appointment(callback: CallbackQuery):
+async def start_appointment(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer("Это запись")
+    await state.set_state(Clinic.specialization)
     await callback.answer()
+
+@router.message(Clinic.specialization, F.text.in_(specialization_arr))
 
 @router.callback_query(F.data == "my_notes")
 async def start_appointment(callback: CallbackQuery):
